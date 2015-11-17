fcadf7c3959cfd12c2b232a6a709f44d8e69efe5
Ygor Kunsch <ygorkrs@gmail.com>
2015-11-17 18:47:34 -0200

    v1.0

---
+++ SYSGYM/Inicial.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace SYSGYM
+{
+    partial class Inicial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicial));
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.bnt_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.n_matricula = new System.Windows.Forms.TextBox();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(71, 148);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(79, 38);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // bnt_2
+            // 
+            this.bnt_2.Location = new System.Drawing.Point(215, 148);
+            this.bnt_2.Name = "bnt_2";
+            this.bnt_2.Size = new System.Drawing.Size(79, 38);
+            this.bnt_2.TabIndex = 1;
+            this.bnt_2.Text = "2";
+            this.bnt_2.UseVisualStyleBackColor = true;
+            this.bnt_2.Click += new System.EventHandler(this.bnt_2_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(365, 148);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(79, 38);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.Location = new System.Drawing.Point(215, 202);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(79, 38);
+            this.btn_5.TabIndex = 3;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.btn_5_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.Location = new System.Drawing.Point(71, 202);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(79, 38);
+            this.btn_4.TabIndex = 4;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.Location = new System.Drawing.Point(365, 202);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(79, 38);
+            this.btn_6.TabIndex = 5;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.btn_6_Click);
+            // 
+            // btn_9
+            // 
+            this.btn_9.Location = new System.Drawing.Point(365, 256);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(79, 38);
+            this.btn_9.TabIndex = 8;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.Click += new System.EventHandler(this.btn_9_Click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.Location = new System.Drawing.Point(215, 256);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(79, 38);
+            this.btn_8.TabIndex = 7;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.Click += new System.EventHandler(this.btn_8_Click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.Location = new System.Drawing.Point(71, 256);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(79, 38);
+            this.btn_7.TabIndex = 6;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.Click += new System.EventHandler(this.btn_7_Click);
+            // 
+            // btn_0
+            // 
+            this.btn_0.Location = new System.Drawing.Point(215, 309);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(79, 38);
+            this.btn_0.TabIndex = 9;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = true;
+            this.btn_0.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(152, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 22);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Matrícula:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // n_matricula
+            // 
+            this.n_matricula.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n_matricula.Location = new System.Drawing.Point(268, 55);
+            this.n_matricula.Name = "n_matricula";
+            this.n_matricula.Size = new System.Drawing.Size(166, 27);
+            this.n_matricula.TabIndex = 11;
+            this.n_matricula.WordWrap = false;
+            this.n_matricula.TextChanged += new System.EventHandler(this.n_matricula_TextChanged);
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Location = new System.Drawing.Point(71, 309);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(79, 38);
+            this.btn_limpar.TabIndex = 12;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click_1);
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.BackColor = System.Drawing.Color.PaleGreen;
+            this.btn_ok.Location = new System.Drawing.Point(365, 309);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(79, 38);
+            this.btn_ok.TabIndex = 13;
+            this.btn_ok.Text = "OK !";
+            this.btn_ok.UseVisualStyleBackColor = false;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // Inicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 392);
+            this.Controls.Add(this.btn_ok);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.n_matricula);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_0);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.bnt_2);
+            this.Controls.Add(this.btn_1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Inicial";
+            this.Text = "SYSGYM";
+            this.Load += new System.EventHandler(this.Inicial_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button bnt_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox n_matricula;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.Button btn_ok;
+    }
+}
+
---
+++ SYSGYM/Inicial.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SYSGYM
+{
+    public partial class Inicial : Form
+    {
+        public Inicial()
+        {
+            InitializeComponent();
+        }
+
+        int num_matricula;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Inicial_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            n_matricula.Text = n_matricula.Text + "1";
+        }
+
+        private void n_matricula_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void bnt_2_Click(object sender, EventArgs e)
+        {
+            n_matricula.Text = n_matricula.Text + "2";
+        }
+
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            n_matricula.Text = n_matricula.Text + "3";
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+            n_matricula.Text = n_matricula.Text + "4";
+        }
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+            n_matricula.Text = n_matricula.Text + "5";
+        }
+
+        private void btn_6_Click(object sender, EventArgs e)
+        {
+            n_matricula.Text = n_matricula.Text + "6";
+        }
+
+        private void btn_7_Click(object sender, EventArgs e)
+        {
+            n_matricula.Text = n_matricula.Text + "7";
+        }
+
+        private void btn_8_Click(object sender, EventArgs e)
+        {
+            n_matricula.Text = n_matricula.Text + "8";
+        }
+
+        private void btn_9_Click(object sender, EventArgs e)
+        {
+            n_matricula.Text = n_matricula.Text + "9";
+        }
+
+        private void btn_0_Click(object sender, EventArgs e)
+        {
+            n_matricula.Text = n_matricula.Text + "0";
+        }
+
+        private void btn_limpar_Click_1(object sender, EventArgs e)
+        {
+            n_matricula.Text = "";
+        }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            if (n_matricula.Text == "")
+            {
+                num_matricula = 0;
+            }
+            else
+                num_matricula = Convert.ToInt32(n_matricula.Text);
+
+
+            if (num_matricula == 123)
+            {
+                Cadastrar_Aluno Cadastar = new Cadastrar_Aluno();
+                Cadastar.Show();
+                Hide();
+            }
+            else
+            {
+                MessageBox.Show("Numero de matrícula inválido!");
+                n_matricula.Text = "";
+            }
+        }
+    }
+}
